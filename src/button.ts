31517d5bedb4902f50f9779811536f440df211b0
Eugene Bravov <eugene.bravov@gmail.com>
2021-02-22 12:46:49 +0300

    bug fix

--- src/button.ts
+++ src/button.ts
@@ -6,7 +6,7 @@ export class Button {
 
     private buildButton() {
         let css = require('./button.scss').default as string;
-        css = css.replace(/^button\s{/, '').replace(/}$/, '');
+        css = css.replace(/^button\s*{/, '').replace(/}$/, '');
 
         this.button = document.createElement('button');
         this.button.setAttribute('style', css);
